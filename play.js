0e4509a774d308448eb8a1b37de6c31201b777e5
Khadija Mohamed <khadijam@live.ca>
2022-01-20 04:56:47 -0500

    refractored + stdin

--- play.js
+++ play.js
@@ -1,4 +1,16 @@
-const net = require("./client");
+const connect = require("./client");
 
 console.log("Connecting ...");
 connect();
+
+
+
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  return stdin;
+};
+
+setupInput();
