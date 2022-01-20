429ac3a2f1d21e1fd744dbfeccf79634ce9ca191
Khadija Mohamed <khadijam@live.ca>
2022-01-20 05:09:19 -0500

    exported to input.js

--- play.js
+++ play.js
@@ -1,23 +1,11 @@
-const connect = require("./client");
+const {connect} = require("./client");
+const { setupInput } = require("./input")
 
 console.log("Connecting ...");
 
-const setupInput = function () {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding("utf8");
-  stdin.resume();
-  return stdin;
-};
-
-const handleUserInput = function () {
-  if (key === '\u0003') {
-    process.exit();
-};
 
 // \u0003 = unicode character of END OF TEXT -- CTRL + C
 
 connect();
 setupInput();
 handleUserInput();
-}
