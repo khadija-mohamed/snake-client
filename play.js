7fd5fc3ac18c9a7cf361f4d2f3e7e723d6469691
Khadija Mohamed <khadijam@live.ca>
2022-01-19 20:01:10 -0500

    Refractored Play

--- play.js
+++ play.js
@@ -1,20 +1,4 @@
-const net = require("net");
-
-// establishes a connection with the game server
-const connect = function () {
-  const conn = net.createConnection({
-    host: '165.227.47.243',
-    port: 50541 
-  });
-
-  // interpret incoming data as text
-  conn.setEncoding("utf8");
-  conn.on("connect", () => {
-    console.log("Connected to game server");
-  })
-
-  return conn;
-};
+const net = require("./client");
 
 console.log("Connecting ...");
 connect();
