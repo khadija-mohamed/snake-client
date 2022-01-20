e15b947713d065d3dc421c3a470feec533d5edb5
Khadija Mohamed <khadijam@live.ca>
2022-01-20 04:57:11 -0500

    moves addded

--- client.js
+++ client.js
@@ -3,6 +3,16 @@ const net = require("net");
 const name = 'Name: KM'
 // establishes a connection with the game server
 const connect = function () {
+  let up = "Move: UP";
+  let down = 'Move: DOWN'; 
+  let left = "Move: LEFT";
+  let right = "Move: RIGHT"; 
+
+  setInterval(() => { conn.write(`${up}`)})
+  setInterval(() => { conn.write(`${down}`)})
+  setInterval(() => { conn.write(`${left}`)})
+  setInterval(() => { conn.write(`${right}`)})
+
   const conn = net.createConnection({
     host: '165.227.47.243',
     port: 50541 
