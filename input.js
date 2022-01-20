e61f786365911ca6dac62ba5b82923f51320dcb3
Khadija Mohamed <khadijam@live.ca>
2022-01-20 05:18:17 -0500

    wasd movements

--- input.js
+++ input.js
@@ -1,15 +1,22 @@
-const setupInput = function () {
+let connection;
+
+const setupInput = (conn) => {
+  connection = conn;
+
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding("utf8");
   stdin.resume();
-  return stdin;
-};
-
-const handleUserInput = function () {
-  if (key === '\u0003') {
-    process.exit();
+  
+  const handleUserInput = function () {
+    if (key === '\u0003') {
+      process.exit();
   }
 }
+
+return stdin;
+}; 
+
+
 
 // \u0003 = unicode character of END OF TEXT -- CTRL + C
