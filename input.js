a9e10f4ead8b48c8473d4ed787615caa9d29a169
Khadija Mohamed <khadijam@live.ca>
2022-01-20 05:14:33 -0500

    exported from play

---
+++ input.js
@@ -0,0 +1,15 @@
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  return stdin;
+};
+
+const handleUserInput = function () {
+  if (key === '\u0003') {
+    process.exit();
+  }
+}
+
+// \u0003 = unicode character of END OF TEXT -- CTRL + C
